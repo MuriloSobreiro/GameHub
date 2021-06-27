a3543810e0aef31971c85fd2da1042dacd5bc812
Murilo Sobreiro <murilosobreiro@hotmail.com>
2021-06-27 12:51:19 -0300

    Inplementada a notação de FEN para a criação de peças

--- GameHubProjetoUnity/Assets/Xadrez/Scripts/GeradorTabuleiro.cs
+++ GameHubProjetoUnity/Assets/Xadrez/Scripts/GeradorTabuleiro.cs
@@ -8,8 +8,9 @@ public class GeradorTabuleiro : MonoBehaviour
 {
     public GameObject casaPrefab, pecaPrefab, pecasGO;
     public Sprite[] imgPecas;
-    public GameObject[,] casasMatriz = new GameObject[8,8];
-    public GameObject[,] pecasMatriz = new GameObject[8,8];
+    public int tam = 8;
+    public GameObject[,] casasMatriz;
+    public GameObject[,] pecasMatriz;
     private RectTransform rtTab;
     public RodadaXadrezControlador rodCont;
     public List<CasaXadrezControlador> casaHighligth = new List<CasaXadrezControlador>();
@@ -17,66 +18,64 @@ public class GeradorTabuleiro : MonoBehaviour
     public PecaXadrezControlador[] reis = new PecaXadrezControlador[2];
     void Start()
     {
+        casasMatriz = new GameObject[tam, tam];
+        pecasMatriz = new GameObject[tam, tam];
         rtTab = GetComponent<RectTransform>();
         rodCont = GetComponentInParent<RodadaXadrezControlador>();
         CriarCasas();
         CriarPecas();
     }
 
-    private void CriarCasas(){
+    private void CriarCasas()
+    {
         float tamanho;
-        tamanho = rtTab.rect.width / 8;
-        bool preto = false;
-        for (int i = 0; i < 8; i++)
+        tamanho = rtTab.rect.width / tam;
+        for (int i = 0; i < tam; i++)
         {
-            for (int j = 0; j < 8; j++)
+            for (int j = 0; j < tam; j++)
             {
                 casasMatriz[i, j] = Instantiate(casaPrefab, transform);
                 RectTransform rt = casasMatriz[i, j].GetComponent<RectTransform>();
                 rt.sizeDelta = new Vector2(tamanho, tamanho);
-                rt.anchoredPosition = new Vector2(tamanho*i, tamanho*j);
-                casasMatriz[i, j].name = ""+((char)(i+65))+(j+1);//Nomeando as Casas
+                rt.anchoredPosition = new Vector2(tamanho * i, tamanho * j);
+                casasMatriz[i, j].name = "" + ((char)(i + 65)) + (j + 1);//Nomeando as Casas
                 casasMatriz[i, j].GetComponent<CasaXadrezControlador>().coordenada = (i, j);
-                if (preto)
-                    casasMatriz[i, j].GetComponent<Image>().color = new Color32(235,236,208,255);
-                preto = !preto;
+                if ((i + j) % 2 == 0)
+                    casasMatriz[i, j].GetComponent<Image>().color = new Color32(235, 236, 208, 255);
             }
-            preto = !preto;
         }
     }
 
     private void CriarPecas()
     {
         pecasGO.GetComponent<RectTransform>().sizeDelta = rtTab.sizeDelta;
-        float tamanho = rtTab.rect.width / 8;
-        for (int i = 0; i < 8; i++)
+        int[,] output = conversorFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+        float tamanho = rtTab.rect.width / tam;
+        for (int i = 0; i < tam; i++)
         {
-            for (int j = 0; j < 8; j++)
+            for (int j = 0; j < tam; j++)
             {
-                var peca = TipoDePeca(i, j);
-                if (peca.valor > 0)
+                if (output[i,j] >= 0)
                 {
-                    peca.valor--;
-                    pecasMatriz[i,j] = Instantiate(pecaPrefab, pecasGO.transform);
-                    Image img = pecasMatriz[i,j].GetComponent<Image>();
-                    img.sprite = imgPecas[peca.valor];
-                    RectTransform rt = pecasMatriz[i,j].GetComponent<RectTransform>();
+                    pecasMatriz[i, j] = Instantiate(pecaPrefab, pecasGO.transform);
+                    RectTransform rt = pecasMatriz[i, j].GetComponent<RectTransform>();
                     rt.sizeDelta = new Vector2(tamanho, tamanho);
                     rt.anchoredPosition = new Vector2(tamanho * i, tamanho * j);
-                    pecasMatriz[i,j].name = peca.tipo;
                     PecaXadrezControlador ctrl = pecasMatriz[i, j].GetComponent<PecaXadrezControlador>();
                     ctrl.coordenada = (i, j);
                     ctrl.gerTab = this;
-                    ctrl.branco = peca.valor > 5 ?  true : false;
-                    if (peca.valor == 1)
+                    ctrl.branco = output[i, j] > 5 ? true : false;
+                    ctrl.tipoPeca = output[i, j];
+                    ctrl.AtualizarPeca();
+                    if (output[i, j] == 1)
                         reis[0] = ctrl;
-                    if (peca.valor == 7)
+                    if (output[i, j] == 7)
                         reis[1] = ctrl;
+                    Image img = pecasMatriz[i, j].GetComponent<Image>();
                     if (ctrl.branco)
                         rodCont.pecasB.Add(img);
                     else
                         rodCont.pecasP.Add(img);
-
                 }
             }
         }
@@ -88,10 +87,10 @@ public class GeradorTabuleiro : MonoBehaviour
         ResetarMovimentos();
         foreach (var coord in coords)
         {
-            if (coord.i < 0 || coord.i > 7 || coord.j < 0 || coord.j > 7)
+            if (coord.i < 0 || coord.i > tam - 1 || coord.j < 0 || coord.j > tam - 1)
                 continue;
             casaHighligth.Add(casasMatriz[coord.i, coord.j].GetComponent<CasaXadrezControlador>());
-            casaHighligth[casaHighligth.Count-1].TrocarCor(pecasMatriz[coord.i,coord.j]!=null);
+            casaHighligth[casaHighligth.Count - 1].TrocarCor(pecasMatriz[coord.i, coord.j] != null);
         }
     }
     public void ResetarMovimentos()
@@ -102,35 +101,61 @@ public class GeradorTabuleiro : MonoBehaviour
         }
         casaHighligth.Clear();
     }
+    private int[,] conversorFen(string input)
+    {
+        int[,] output = new int[tam, tam];
+        string[] temp = input.Split('/');
+        int i;
+        for (int j = 0; j < tam; j++)
+        {
+            i = 0;
+            foreach (char letra in temp[j])
+            {
+                int val = FenParaInt(letra);
+                Debug.Log(i + "," + j);
+                if(val >= 0)
+                    output[i, j] = val;
+                else
+                {
+                    for (int a = 0; a < (int)Char.GetNumericValue(letra); a++)
+                    {
+                        output[i+a, j] = val;
+                    }
+                    i += (int)Char.GetNumericValue(letra)-1;
+                }
+                i++;
+            }
+        }
+        return output;
+    }
 
-    private (int valor,string tipo) TipoDePeca(int i, int j)
-    {//1&7 Rainhas, 2&8 Reis, 3&9 Torres, 4&10 Cavalos, 5&11 Bispos, 6&12 Peões
-        if(j>1 && j<6)//Não há peças nas linhas de idex 2 a 5
-            return (0,"");
-        if (j == 1)//As linhas de index 1 e 6 são paredes de peões
-            return (12, "PeaoB");
-        if (j == 6)
-            return (6,"PeaoP");
-        if ((i == 0 || i == 7) && j == 0)//Posição das Torres
-            return (9, "Torre");
-        if ((i == 0 || i == 7) && j == 7)
-            return (3, "Torre");
-        if ((i == 1 || i == 6) && j == 0)//Posição dos Cavalos
-            return (10, "Cavalo");
-        if ((i == 1 || i == 6) && j == 7)
-            return (4, "Cavalo");
-        if ((i == 2 || i == 5) && j == 0)//Posição dos Bispos
-            return (11, "Bispo");
-        if ((i == 2 || i == 5) && j == 7)
-            return (5, "Bispo");
-        if (i == 3 && j == 0)//Posição da Rainha Branca
-            return (7, "Rainha");
-        if (i == 3 && j == 7)//Posição da Rainha Preta
-            return (1, "Rainha");
-        if (i == 4 && j == 0)//Posição do Rei Branco
-            return (8, "Rei");
-        if (i == 4 && j == 7)//Posição do Rei Preto
-            return (2, "Rei");
-        return (0,"");
+    private int FenParaInt(char a)
+    {
+        int result = -7;
+        switch (Char.ToUpper(a))
+        {
+            case 'Q':
+                result = 0;
+                break;
+            case 'K':
+                result = 1;
+                break;
+            case 'R':
+                result = 2;
+                break;
+            case 'N':
+                result = 3;
+                break;
+            case 'B':
+                result = 4;
+                break;
+            case 'P':
+                result = 5;
+                break;
+            default:
+                break;
+        }
+        result += Char.IsUpper(a) ? 0 : 6;
+        return result;
     }
 }
